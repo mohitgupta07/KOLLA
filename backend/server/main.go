880f1d198f1be4a1f73a8a08c343bf6d112852a9
mohitgupta07 <cse.mohitgupta.07@gmail.com>
2024-08-10 20:22:34 +0530

    backend fix

--- backend/server/main.go
+++ backend/server/main.go
@@ -5,24 +5,23 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
-	"google.golang.org/api/oauth2/v2"
 )
 
 var (
 	jwtKey            = []byte("your_secret_key")
 	googleOauthConfig = &oauth2.Config{
-		RedirectURL:  "http://localhost:3000/auth/callback",
+		RedirectURL:  "http://localhost:8080/callback",
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+		Scopes:       []string{"profile", "email"},
 		Endpoint:     google.Endpoint,
 	}
+	oauth2Token *oauth2.Token
 )
 
 type Claims struct {
@@ -33,63 +32,78 @@ type Claims struct {
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/auth/login", handleGoogleLogin).Methods("GET")
-	router.HandleFunc("/auth/callback", handleGoogleCallback).Methods("GET")
+	router.HandleFunc("/auth", handleAuth)
+	router.HandleFunc("/callback", handleCallback)
 	router.HandleFunc("/data", handleData).Methods("GET", "POST", "PUT")
 	router.HandleFunc("/generate-pdf", handleGeneratePDF).Methods("POST")
-
+	http.Handle("/", router)
 	log.Println("Server started at :8080")
 	http.ListenAndServe(":8080", router)
 }
 
-func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
-	url := googleOauthConfig.AuthCodeURL("randomstate")
+func handleAuth(w http.ResponseWriter, r *http.Request) {
+	url := googleOauthConfig.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
 	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
 
-func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
+func handleCallback(w http.ResponseWriter, r *http.Request) {
 	code := r.URL.Query().Get("code")
 	token, err := googleOauthConfig.Exchange(context.Background(), code)
 	if err != nil {
-		http.Error(w, "Failed to exchange token", http.StatusInternalServerError)
-		return
+		log.Fatal(err)
 	}
-
-	oauth2Service, err := oauth2.NewService(context.Background(), oauth2.TokenSource(token))
-	if err != nil {
-		http.Error(w, "Failed to create OAuth2 service", http.StatusInternalServerError)
-		return
-	}
-
-	userInfo, err := oauth2Service.Userinfo.Get().Do()
-	if err != nil {
-		http.Error(w, "Failed to get user info", http.StatusInternalServerError)
-		return
-	}
-
-	// Create JWT token
-	expirationTime := time.Now().Add(24 * time.Hour)
-	claims := &Claims{
-		Email: userInfo.Email,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: expirationTime.Unix(),
-		},
-	}
-
-	tokenString, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString(jwtKey)
-	if err != nil {
-		http.Error(w, "Failed to create JWT", http.StatusInternalServerError)
-		return
-	}
-
-	http.SetCookie(w, &http.Cookie{
-		Name:    "token",
-		Value:   tokenString,
-		Expires: expirationTime,
-	})
-
+	oauth2Token = token
 	http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 }
+
+// func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
+// 	url := googleOauthConfig.AuthCodeURL("randomstate")
+// 	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+// }
+
+// func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
+// 	code := r.URL.Query().Get("code")
+// 	token, err := googleOauthConfig.Exchange(context.Background(), code)
+// 	if err != nil {
+// 		http.Error(w, "Failed to exchange token", http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	oauth2Service, err := oauth2.NewService(context.Background(), oauth2.TokenSource(token))
+// 	if err != nil {
+// 		http.Error(w, "Failed to create OAuth2 service", http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	userInfo, err := oauth2Service.Userinfo.Get().Do()
+// 	if err != nil {
+// 		http.Error(w, "Failed to get user info", http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	// Create JWT token
+// 	expirationTime := time.Now().Add(24 * time.Hour)
+// 	claims := &Claims{
+// 		Email: userInfo.Email,
+// 		StandardClaims: jwt.StandardClaims{
+// 			ExpiresAt: expirationTime.Unix(),
+// 		},
+// 	}
+
+// 	tokenString, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString(jwtKey)
+// 	if err != nil {
+// 		http.Error(w, "Failed to create JWT", http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	http.SetCookie(w, &http.Cookie{
+// 		Name:    "token",
+// 		Value:   tokenString,
+// 		Expires: expirationTime,
+// 	})
+
+// 	http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+// }
 
 func handleData(w http.ResponseWriter, r *http.Request) {
 	// Check JWT token
