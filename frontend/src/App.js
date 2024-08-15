5dacc2547164de187e8759c3b7753325a445523e
mohitgupta07 <cse.mohitgupta.07@gmail.com>
2024-08-16 02:06:52 +0530

    minor ui update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
+import { AppBar, Toolbar, Typography, Button, Container, CssBaseline } from '@mui/material';
 
 function App() {
     const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
@@ -23,12 +24,30 @@ function App() {
 
     return (
         <GoogleOAuthProvider clientId={clientId}>
-            <div className="App">
+            <CssBaseline />
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        MyApp
+                    </Typography>
+                    <Button color="inherit">Home</Button>
+                    <Button color="inherit">About</Button>
+                    <Button color="inherit">Contact</Button>
+                </Toolbar>
+            </AppBar>
+            <Container maxWidth="sm" sx={{ mt: 4, textAlign: 'center' }}>
+                <Typography variant="h4" gutterBottom>
+                    Welcome to MyApp
+                </Typography>
                 <GoogleLogin
                     onSuccess={onSuccess}
                     onError={onFailure}
+                    style={{ margin: '20px 0' }}
                 />
-            </div>
+                <Typography variant="body1">
+                    Sign in with Google to access more features.
+                </Typography>
+            </Container>
         </GoogleOAuthProvider>
     );
 }
